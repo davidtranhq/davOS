ab9adf2f88a105bc6db86697d46ba36aec6c8c4a
David Tran <davidtranhq@gmail.com>
2022-11-04 23:53:52 -0400

    Test global constructors

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -2,12 +2,22 @@
 
 extern "C"
 {
-
 #include <kernel/tty.h>
+
+void kernel_main(void);
+}
+
+struct GlobalConstructorTest
+{
+	GlobalConstructorTest() : c {'X'} {}
+	char c;
+};
+
+GlobalConstructorTest g;
 
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
-}
+	printf("Hello, kernel!\n");
 
+	printf("%c", g.c);
 }
