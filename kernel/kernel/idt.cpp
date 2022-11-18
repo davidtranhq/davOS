62b1e2cd0f4a8cdde09e333fd87f6f6a5c2a22a8
David Tran <davidtranhq@gmail.com>
2022-11-18 02:46:58 -0500

    [WIP] Loading IDT into memory

--- kernel/kernel/idt.cpp
+++ kernel/kernel/idt.cpp
@@ -1,20 +1,33 @@
+#include <string.h>
+
 #include <kernel/GateDescriptor.h>
 #include <kernel/idt.h>
+#include <kernel/kernel.h>
 #include <kernel/SegmentSelector.h>
+
+__attribute__((interrupt))
+void isr_divide_by_zero(void *interrupt_frame)
+{
+    kernel_panic("divide by zero\n");
+}
 
 void idt_init()
 {
-    constexpr auto selector = SegmentSelector(PrivilegeLevel::user, DescriptorTable::local, 0xabc0);
-    
-    constexpr auto descriptor = GateDescriptor(
-        0xabcd1234abcd1234,
-        selector,
-        5,
-        GateDescriptor::GateType::trap,
-        PrivilegeLevel::user
-    ).to_bytes();
+    GateDescriptor de_descriptor(
+        reinterpret_cast<uint64_t>(&isr_divide_by_zero),
+        SegmentSelector(
+            PrivilegeLevel::kernel,
+            DescriptorTable::global,
+            0
+        ),
+        0,
+        GateDescriptor::GateType::interrupt,
+        PrivilegeLevel::kernel
+    );
 
-    // TEST: check that SegmentSelector and GateDescriptor are indeed compile-time constructs
-    [[ maybe_unused ]]
-    constexpr auto sum = descriptor[0] + descriptor[1] + descriptor[2] + descriptor[3];
+    void *idt_base = reinterpret_cast<void *>(0x1000);
+    memcpy(idt_base, de_descriptor.to_bytes(), GateDescriptor::NUM_BYTES);
+
+    // WRONG: this should be a linear address
+    __asm__("lidt 0x1000");
 }
