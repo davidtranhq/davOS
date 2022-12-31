ec595735d04d0e240eb32f3a646b0d9433f01a71
David Tran <davidtranhq@gmail.com>
2022-12-31 14:10:30 -0500

    Print the loaded memory map

--- kernel/kernel/main.cpp
+++ kernel/kernel/main.cpp
@@ -12,12 +12,21 @@ void kernel_main(void);
 
 END_CDECLS
 
+struct limine_memmap_request request = {
+    .id = LIMINE_MEMMAP_REQUEST,
+    .revision = 0
+};
+
 void kernel_main()
 {
     kernel_init();
     
     printf("Hello, world!\n");
-    test_interrupt_divide_by_zero();
+    printf("Memory Map:\n");
+    for (uint64_t i = 0; i < request.response->entry_count; ++i) {
+        auto entry = request.response->entries[i];
+        printf("base: %llu, length: %llu, type: %llu\n", entry->base, entry->length, entry->type);
+    }
 
     kernel_hang();
 }
