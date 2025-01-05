3d41cad2d67e6d2cfb0b0d34fdb8b6e4edde0790
davidtranhq <davidtranhq@gmail.com>
2025-01-04 22:01:28 -0500

    Use int instead of bool for strncmp

--- kernel/lib/kernelc/string/strncmp.cpp
+++ kernel/lib/kernelc/string/strncmp.cpp
@@ -1,9 +1,9 @@
 #include <string.h>
 
-bool strncmp(const char *a, const char *b, size_t size) {
+int strncmp(const char *a, const char *b, size_t size) {
     for (size_t i = 0; i < size; i++) {
         if (a[i] != b[i])
-            return false;
+            return a[i] - b[i];
     }
-    return true;
+    return 0;
 }
